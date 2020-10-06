1628aaf7e481308b0866f2feda42797ced1f5035
Cheryl Gardner <cgardner3@hawk.iit.edu>
2020-10-06 11:29:20 -0500

    Add a confirmation box function

--- js/script.js
+++ js/script.js
@@ -5,4 +5,13 @@ jQuery(function($) {
 function clearData(){
 	document.getElementById("form1").reset();
 }
+function confirmJoin(){
+	var h =confirm("Are you sure you are ready to join the fan club?")
+	if (h == true){
+		alert("Welcome to the fan club")
+	}
+	else{
+		alert("We hope that you will join us soon!!")
+	}
+}
 document.getElementById("submit-button").addEventListener("click",clearData());
