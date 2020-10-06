e7d1da95946059901ff41822d5e265221c6369fd
Cheryl Gardner <cgardner3@hawk.iit.edu>
2020-10-06 11:34:11 -0500

    Add missing semicolons

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ function clearData(){
 	document.getElementById("form1").reset();
 }
 function confirmJoin(){
-	var h =confirm("Are you sure you are ready to join the fan club?")
+	var h = confirm("Are you sure you are ready to join the fan club?");
 	if (h == true){
-		alert("Welcome to the fan club")
+		alert("Welcome to the fan club");
 	}
 	else{
-		alert("We hope that you will join us soon!!")
+		alert("We hope that you will join us soon!!");
 	}
 }
 document.getElementById("submit-button").addEventListener("click",confirmJoin());
