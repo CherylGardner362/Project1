731b3680a660e0b2d77552db27c4988e0b5cc445
Cheryl Gardner <cgardner3@hawk.iit.edu>
2020-10-06 17:40:51 -0500

    Put back javascript to original and add an onclick function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-function welcomeMessage(){
-	alert('Welcome to the Fan Club');
-}
+function confirmJoin(){
+	var answer = confrim("Are you ready to join the fan club?");
+	if (answer == true) {
+		alert("Welcome to the club!!!");
+	}
+	else{
+		alert("We hope you join us soon!!");
+	}
 
-var button = document.getElementById('submit-button');
-button.addEventListener('click',welcomeMessage);
+	var q1 = document.getElementById('submit-button');
+	q1.addEventListener('click',confirmJoin)
