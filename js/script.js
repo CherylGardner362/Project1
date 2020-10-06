1fdecd35bfb8ebef9ef59db0ec945f22b2cda122
Cheryl Gardner <cgardner3@hawk.iit.edu>
2020-10-06 17:21:33 -0500

    Restart javascript

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-
-function clearData(){
-	document.getElementById("form1").reset();
-}
-function confirmJoin(){
-	var h = confirm("Are you sure you are ready to join the fan club?");
-	if (h == true){
-		alert("Welcome to the fan club");
+function confirmJoin() {
+	var answer = prompt("Are you ready to join the fan club?");
+	if (answer == true) {
+		alert("Welcome to the club!!!");
 	}
 	else{
-		alert("We hope that you will join us soon!!");
+		alert("We hope you join us soon!!");
 	}
 }
+
 document.getElementById("submit-button").addEventListener("click",confirmJoin());
-//document.getElementById("submit-button").addEventListener("click",clearData());
