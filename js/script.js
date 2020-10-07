a3d2d35baae1a466b9eab7b331cc7e0914e8290e
Cheryl Gardner <cgardner3@hawk.iit.edu>
2020-10-07 14:23:17 -0500

    Add required field and delete some javascript

--- js/script.js
+++ js/script.js
@@ -7,25 +7,6 @@ function confirmJoin(){
 		alert("We hope you join us soon!!");
 	}
 }
-
-function validationEvent() {
-	var a = document.forms["form1"]["full_name"].value;
-	if (a == "") {
-		alert("Please enter your full name");
-		return false;
-	}
-	var b = document.forms["form1"]["email"].value;
-	if (b == "") {
-		alert("Please enter your email address");
-		return false;
-	}
-	var c = document.forms["form1"]["fav_team"].value;
-	if (c == "") {
-		alert("Please pick a team");
-		return false;
-	}
-}
 	
 	var q1 = document.getElementById('submit-button');
 	q1.addEventListener('click',confirmJoin)
-	q1.addEventListener('click',validationEvent)
