1125c84abc37d62d73c56063f17b043a8af1f088
Cheryl Gardner <cgardner3@hawk.iit.edu>
2020-10-07 13:39:35 -0500

    Add a second javascript and link to validation event

--- script.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($) {
-	$('html').removeClass('nojs');
-	$('html').addClass('hasjs');
-});
-function clearData(){
-	document.getElementById("form1").reset();
-}
-document.getElementById("submit-button").addEventListener("click",clearData());
