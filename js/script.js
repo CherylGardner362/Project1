bac59c9ecb6e1f73c6be093c3ef00485b44637bc
Cheryl Gardner <cgardner3@hawk.iit.edu>
2020-10-07 14:38:32 -0500

    Rework javascript

--- js/script.js
+++ js/script.js
@@ -8,5 +8,17 @@ function confirmJoin(){
 	}
 }
 	
+function validationEvent() {
+	var a = document.forms["form1"]["full_name"].value;
+	var b = document.forms["form1"]["email"].value;
+	var c = document.forms["form1"]["fav_team"].value;
+	if (a == "" || b == "" || c == ""){
+		alert("All of the Data Fields must be filled out")
+	}
+	else{
+		confirmJoin();
+	}
+}
+
 	var q1 = document.getElementById('submit-button');
-	q1.addEventListener('click',confirmJoin)
+	q1.addEventListener('click',validationEvent)
